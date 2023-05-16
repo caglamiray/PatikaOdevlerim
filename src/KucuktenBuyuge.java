712e3f2eeba5f0adb90258e7712dcac9d8d42014
caglamiray <cagla.mirayy@gmail.com>
2023-05-16 14:22:29 +0300

    KdvHesap

---
+++ src/KucuktenBuyuge.java
@@ -0,0 +1,37 @@
+import javax.security.sasl.SaslServer;
+import java.util.Scanner;
+public class KucuktenBuyuge {
+    public static void main(String[] args) {
+
+        int a,b,c;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("1.sayı: ");
+        a = input.nextInt();
+        System.out.print("2.sayı: ");
+        b= input.nextInt();
+        System.out.print("3.sayı: ");
+        c = input.nextInt();
+
+        if ((a<b) && (a<c)) {
+            if (b<c) {
+                System.out.println("a<b<c");
+            }else{
+                System.out.println("a<c<b");
+            }
+        } else if ((b<a) && (b<c)) {
+            if (a<c) {
+                System.out.println("b<a<c");
+            }else{
+                System.out.println("b<c<a");
+            }
+        }else {
+            if (b<a) {
+                System.out.println("c<b<a");
+            } else {
+                System.out.println("c<a<b");
+            }
+        }
+    }
+}
