f43b0ff01f9e2ee77cde39b7baeea3ee39ac7a76
caglamiray <cagla.mirayy@gmail.com>
2023-06-05 21:30:13 +0300

    PalindromKelime

---
+++ Hafta02/PalindromKelime/src/Main.java
@@ -0,0 +1,22 @@
+import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
+
+public class Main {
+
+    static boolean isPalindrome(String str) {
+        int i=0;
+        int j= str.length()-1;
+
+        while (i<j) {
+
+            if (str.charAt(i)!=str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("sdfg"));
+    }
+}
